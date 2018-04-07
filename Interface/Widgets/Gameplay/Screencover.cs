6e733e0688c511746ebb5e0300251f6c04f63a7d
Percyqaz <percyqaz@yahoo.co.uk>
2018-04-07 15:04:25 +0100

    screencover fix - i was dumb - but not really

--- Interface/Widgets/Gameplay/Screencover.cs
+++ Interface/Widgets/Gameplay/Screencover.cs
@@ -8,12 +8,12 @@ namespace YAVSRG.Interface.Widgets.Gameplay
 {
     public class Screencover : GameplayWidget
     {
-        bool down;
+        bool flip;
         Sprite texture;
 
         public Screencover(YAVSRG.Gameplay.ScoreTracker st, bool d) : base(st)
         {
-            down = d;
+            flip = d;
             texture = Content.LoadTextureFromAssets("screencover");
         }
 
@@ -21,15 +21,15 @@ namespace YAVSRG.Interface.Widgets.Gameplay
         {
             base.Draw(left, top, right, bottom);
             ConvertCoordinates(ref left, ref top, ref right, ref bottom);
-            if (down)
+            if (flip)
             {
-                SpriteBatch.Draw(texture, left, bottom - Game.Options.Theme.ColumnWidth, right, bottom, System.Drawing.Color.White, 0, 0, 2);
-                SpriteBatch.Draw(texture, left, bottom - Game.Options.Theme.ColumnWidth, right, top, System.Drawing.Color.White, 0, 1);
+                SpriteBatch.Draw(texture, left, top + Game.Options.Theme.ColumnWidth, right, bottom, System.Drawing.Color.White, 0, 1);
+                SpriteBatch.Draw(texture, left, top, right, top + Game.Options.Theme.ColumnWidth, System.Drawing.Color.White, 0, 0);
             }
             else
             {
-                SpriteBatch.Draw(texture, left, top + Game.Options.Theme.ColumnWidth, right, top, System.Drawing.Color.White, 0, 0, 2);
-                SpriteBatch.Draw(texture, left, top + Game.Options.Theme.ColumnWidth, right, bottom, System.Drawing.Color.White, 0, 1);
+                SpriteBatch.Draw(texture, left, top, right, bottom - Game.Options.Theme.ColumnWidth, System.Drawing.Color.White, 0, 1);
+                SpriteBatch.Draw(texture, left, bottom - Game.Options.Theme.ColumnWidth, right, bottom, System.Drawing.Color.White, 0, 0, 2);
             }
             //SpriteBatch.Draw(texture, left, h * amount - COLUMNWIDTH - Height, right, h * amount - Height, Color.White, 0, 0, 2);
             //SpriteBatch.Draw(texture, left, -Height, right, h * amount - COLUMNWIDTH - Height, Color.White, 0, 1, 2);
