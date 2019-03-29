0230bf5af0cd44027addd3d8f3a5e2d5bd945f0b
Percyqaz <percyqaz@yahoo.co.uk>
2019-03-29 20:54:12 +0000

    small refactor in advance of big refactor - separated stuff into files properly - removed the library for p2p since it will no longer be used

--- IO/Net/P2P/P2PManager.cs
+++ IO/Net/P2P/P2PManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Open.Nat;
 using Interlude.Utilities;
 using Interlude.Net.P2P.Protocol.Packets;
 
