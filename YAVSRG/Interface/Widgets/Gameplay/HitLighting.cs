628e968bf8cbaed2a9f108ec712981c64f686544
Percyqaz <percyqaz@yahoo.co.uk>
2019-07-24 16:26:17 +0100

    hitlighting is fixed but not complete because upscroll doesn't work - todo: fix upscroll positioning of hitlights - todo: option to disable them - todo: easy theme list selector

--- YAVSRG/Interface/Widgets/Gameplay/HitLighting.cs
+++ YAVSRG/Interface/Widgets/Gameplay/HitLighting.cs
@@ -7,7 +7,6 @@ namespace Interlude.Interface.Widgets.Gameplay
 {
     class HitLighting : Widget
     {
-        AnimationSlider NoteLight = new AnimationSlider(0);
         AnimationSlider ReceptorLight = new AnimationSlider(0);
         Bind KeyBind;
         float scale;
@@ -16,13 +15,12 @@ namespace Interlude.Interface.Widgets.Gameplay
         {
             KeyBind = bind;
             scale = Game.Options.Theme.ColumnWidth / Game.Options.Themes.GetTexture("receptorlighting").Width;
-            Animation.Add(NoteLight);
             Animation.Add(ReceptorLight);
         }
 
         public override void Update(Rect bounds)
         {
-            ReceptorLight.Target = KeyBind.Held() ? 1 : 0;
+            if (KeyBind.Held()) ReceptorLight.Val = 1;
             base.Update(bounds);
         }
 
@@ -31,14 +29,11 @@ namespace Interlude.Interface.Widgets.Gameplay
             base.Draw(bounds);
             bounds = GetBounds(bounds);
             float w = bounds.Width;
-            if (ReceptorLight.Val > Game.Options.Theme.ColumnLightTime)
+            float threshold = 1 - Game.Options.Theme.ColumnLightTime;
+            if (ReceptorLight.Val > threshold)
             {
-                int a = (int)(255 * (ReceptorLight.Val - Game.Options.Theme.ColumnLightTime) / Game.Options.Theme.ColumnLightTime);
-                SpriteBatch.DrawAlignedTexture("receptorlighting", bounds.CenterX, bounds.Top, scale * ReceptorLight.Val, scale / ReceptorLight.Val, -0.5f, 0, Color.FromArgb(a, Color.White));
-            }
-            if (NoteLight.Val > 0f)
-            {
-                SpriteBatch.Draw(new RenderTarget(Game.Options.Themes.GetTexture("notelighting"), bounds.SliceBottom(w), Color.FromArgb((int)(NoteLight.Val * 255), Color.White)));
+                int a = (int)(255f * (ReceptorLight.Val - threshold) / Game.Options.Theme.ColumnLightTime);
+                SpriteBatch.DrawAlignedTexture("receptorlighting", bounds.CenterX, bounds.Top, scale * ReceptorLight.Val, scale / ReceptorLight.Val, -0.5f, -1, Color.FromArgb(a, Color.White));
             }
         }
     }
