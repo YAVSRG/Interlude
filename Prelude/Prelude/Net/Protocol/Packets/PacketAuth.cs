0cbe0a8599f8c095b01687bc8e033bb35a734aea
Percy Qaz <percyqaz@yahoo.co.uk>
2020-03-08 23:57:53 +0000

    prelude is now a submodule

--- Prelude/Prelude/Net/Protocol/Packets/PacketAuth.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Prelude.Net.Protocol.Packets
-{
-    public class PacketAuth : Packet<PacketAuth>
-    {
-        public string username;
-        public string passkey;
-        public int protocolversion;
-    }
-}
