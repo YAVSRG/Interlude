c0eba5444499a51e3789e2c49aec62f2e9ac7513
Percyqaz <percyqaz@yahoo.co.uk>
2020-04-16 17:25:52 +0100

    launcher and blank game window

---
+++ Interlude/Game.fs
@@ -0,0 +1,16 @@
+﻿namespace Interlude
+
+open OpenTK
+open OpenTK.Graphics
+
+module Game = 
+    let version = "v0.4.0"
+
+type Game() =
+    inherit GameWindow(300, 300, new GraphicsMode(ColorFormat(32), 24, 8, 0, ColorFormat(0)))
+
+    do
+        base.Title <- "Interlude " + Game.version
+
+    override this.OnRenderFrame (e) =
+        base.OnRenderFrame (e)
