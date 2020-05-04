99359ba914f2185b2b524025773acb4a2b8b9693
Percyqaz <percyqaz@yahoo.co.uk>
2020-05-04 15:05:56 +0100

    more screen logic and options saving/loading

--- Interlude/UI/Screens.fs
+++ Interlude/UI/Screens.fs
@@ -1,2 +1,30 @@
-﻿module Screens
+﻿namespace Interlude.UI
 
+type Screen() =
+    inherit Widget()
+    member this.OnEnter(prev: Screen) = ()
+    member this.OnExit(next: Screen) = ()
+
+type ScreenMenu() =
+    inherit Screen()
+
+type ScreenContainer() =
+    inherit Widget()
+
+    let mutable dialogs = []
+    let mutable current = new ScreenMenu() :> Screen
+    let mutable screens = [current]
+
+    let AddScreen(s: Screen) =
+        screens <- s :: screens
+        current.OnExit(s)
+        s.OnEnter(current)
+        current <- s
+
+    let RemoveScreen() =
+        screens <- List.tail screens
+        match List.tryHead screens with
+        | None -> ()
+        | Some s ->
+            current.OnExit(s)
+            s.OnEnter(current)
