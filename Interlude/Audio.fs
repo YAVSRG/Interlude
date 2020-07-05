503b406e5e2e29e0c9bb5b24ee11aafc1a8b9383
Percyqaz <percyqaz@yahoo.co.uk>
2020-07-05 23:55:02 +0100

    working note renderer - still has a couple features missing but is looking more promising than the c# version already - also updated everything to include new units of measure to match prelude

--- Interlude/Audio.fs
+++ Interlude/Audio.fs
@@ -14,9 +14,9 @@ module Audio =
     type Track = {
         ID: int //id used by Bass
         Frequency: int //frequency the file is encoded in, so it knows how to up and downrate the audio correctly
-        Duration: float //duration of the song so it doesn't have to be tediously recalculated in case of mp3 and stuff
+        Duration: float32<ms> //duration of the song so it doesn't have to be tediously recalculated in case of mp3 and stuff
     } with
-        static member Default = { ID = 0; Frequency = 1; Duration = 1000.0 }
+        static member Default = { ID = 0; Frequency = 1; Duration = 1000.0f<ms> }
         static member FromFile(file: string) =
             //let ID = Bass.CreateStream(file, int64 0, int64 0, BassFlags.Decode); //loads file
             let ID = Bass.CreateStream(file); //loads file
@@ -28,7 +28,7 @@ module Audio =
                 let Duration = Bass.ChannelBytes2Seconds(ID, Bass.ChannelGetLength(ID)) * 1000.0
                 let Frequency = d.Frequency
                 //let ID = BassFx.TempoCreate(ID, BassFlags.FxFreeSource)
-                { ID = ID; Frequency = Frequency; Duration = Duration }
+                { ID = ID; Frequency = Frequency; Duration = Duration |> toTime }
         member this.Dispose() =
             Bass.StreamFree(this.ID) |> bassError
 
@@ -37,21 +37,21 @@ module Audio =
     | Wait
     | Action of (unit -> unit)
 
-    let private LEADIN_TIME = 2000.0
+    let private LEADIN_TIME = 2000.0f<ms>
     
     let mutable private nowplaying: Track = Track.Default
     let private fft: float32 array = Array.zeroCreate 1024
     let waveForm: float32 array = Array.zeroCreate 256
     let private timer = new Stopwatch()
-    let mutable private timerStart = 0.0
+    let mutable private timerStart = 0.0f<ms>
     let mutable private channelPlaying = false
-    let mutable private rate = 1.0
-    let mutable private localOffset = 0.0
-    let mutable globalOffset = 0.0
+    let mutable private rate = 1.0f
+    let mutable private localOffset = 0.0f<ms>
+    let mutable globalOffset = 0.0f<ms>
 
     let audioDuration() = nowplaying.Duration
 
-    let time() = rate * (float timer.ElapsedMilliseconds) + timerStart
+    let time() = rate * (float32 timer.ElapsedMilliseconds * 1.0f<ms>) + timerStart
 
     let timeWithOffset() = time() + localOffset + globalOffset * rate
 
@@ -59,9 +59,9 @@ module Audio =
 
     let playFrom(time) =
         timerStart <- time
-        if (time > 0.0 && time < audioDuration()) then
+        if (time > 0.0f<ms> && time < audioDuration()) then
             channelPlaying <- true
-            Bass.ChannelSetPosition(nowplaying.ID, Bass.ChannelSeconds2Bytes(nowplaying.ID, time / 1000.0)) |> bassError
+            Bass.ChannelSetPosition(nowplaying.ID, Bass.ChannelSeconds2Bytes(nowplaying.ID, float <| time / 1000.0f<ms>)) |> bassError
             Bass.ChannelPlay(nowplaying.ID) |> bassError
         else
             Bass.ChannelPause(nowplaying.ID) |> bassError
@@ -72,9 +72,9 @@ module Audio =
 
     let seek(time) =
         timerStart <- time
-        if (time > 0.0 && time < audioDuration()) then
+        if (time > 0.0f<ms> && time < audioDuration()) then
             channelPlaying <- true
-            Bass.ChannelSetPosition(nowplaying.ID, Bass.ChannelSeconds2Bytes(nowplaying.ID, time / 1000.0)) |> bassError
+            Bass.ChannelSetPosition(nowplaying.ID, Bass.ChannelSeconds2Bytes(nowplaying.ID, float <| time / 1000.0f<ms>)) |> bassError
         else
             Bass.ChannelPause(nowplaying.ID) |> bassError
             channelPlaying <- false
@@ -111,9 +111,9 @@ module Audio =
         updateWaveform()
 
         let t = time()
-        if (t > 0.0 && t < nowplaying.Duration && not channelPlaying) then
+        if (t > 0.0f<ms> && t < nowplaying.Duration && not channelPlaying) then
             channelPlaying <- true
-            Bass.ChannelSetPosition(nowplaying.ID, Bass.ChannelSeconds2Bytes(nowplaying.ID, t / 1000.0)) |> bassError
+            Bass.ChannelSetPosition(nowplaying.ID, Bass.ChannelSeconds2Bytes(nowplaying.ID, float <| t / 1000.0f<ms>)) |> bassError
             Bass.ChannelPlay(nowplaying.ID) |> bassError
         elif (t > nowplaying.Duration) then
             channelPlaying <- false
@@ -122,11 +122,11 @@ module Audio =
     let changeRate(newRate) =
         rate <- newRate;
         //if (PREVENT_PITCH_CHANGE) then Bass.ChannelSetAttribute(nowplaying.ID, ChannelAttribute.Pitch, -Math.Log(rate, 2.0) * 12.0) |> ignore
-        Bass.ChannelSetAttribute(nowplaying.ID, ChannelAttribute.Frequency, float nowplaying.Frequency * rate) |> bassError
+        Bass.ChannelSetAttribute(nowplaying.ID, ChannelAttribute.Frequency, float32 nowplaying.Frequency * rate) |> bassError
 
     let changeTrack(path, offset, rate) =
         if playing() then pause()
-        timerStart <- -infinity
+        timerStart <- -infinityf * 1.0f<ms>
         if nowplaying.ID <> 0 then
             nowplaying.Dispose()
         nowplaying <- Track.FromFile(path)
--- Interlude/UI/Screens/ScreenPlay.fs
+++ Interlude/UI/Screens/ScreenPlay.fs
@@ -1,10 +1,20 @@
 ﻿namespace Interlude.UI
 
 open OpenTK
+open System
+open Prelude.Common
 open Prelude.Charts.Interlude
 open Interlude
 open Interlude.Render
 open Interlude.Options
+
+//TODO LIST
+//  UPSCROLL SUPPORT
+//  ANIMATION SUPPORT
+//  RECEPTORS
+//  COLUMN INDEPENDENT SV
+//  MAYBE FIX HOLD TAIL CLIPPING
+
 
 type NoteRenderer() as this =
     inherit Widget()
@@ -12,12 +22,27 @@ type NoteRenderer() as this =
     
     //functions to get bounding boxes for things. used to place other gameplay widgets on the playfield.
 
-    let (keys, notes, bpm, sv, mods) = Gameplay.modifiedChart.Force()
+    //constants
+    let (keys, notes, bpm, sv, mods) = Gameplay.coloredChart.Force()
     let columnPositions = Array.init keys (fun i -> float32 i * Themes.noteskinConfig.ColumnWidth)
     let columnWidths = Array.create keys (float32 Themes.noteskinConfig.ColumnWidth)
     let noteHeight = Themes.noteskinConfig.ColumnWidth
-    let scale = Options.profile.ScrollSpeed.Get() / Gameplay.rate
+    let scale = float32(Options.profile.ScrollSpeed.Get()) / Gameplay.rate * 1.0f</ms>
     let hitposition = float32 <| Options.profile.HitPosition.Get()
+    let holdnoteTrim = Themes.noteskinConfig.ColumnWidth * 0.4f
+
+    //arrays of stuff that are reused/changed every frame. the data from the previous frame is not used, but making new arrays causes garbage collection
+    let mutable note_seek = 0 //see comments for sv_seek and sv_peek. same role but for index of next row
+    let mutable note_peek = note_seek
+    let sv = Array.init (keys + 1) (fun i -> sv.GetChannelData(i - 1).Data)
+    let sv_seek = Array.create (keys + 1) 0 //index of next appearing SV point for each channel; = number of SV points if there are no more
+    let sv_peek = Array.create (keys + 1) 0 //same as sv_seek, but sv_seek stores the relevant point for t = now (according to music) and this stores t > now
+    let sv_value = Array.create (keys + 1) 1.0f //value of the most recent sv point per channel, equal to the sv value at index (sv_peek - 1), or 1 if that point doesn't exist
+    let sv_time = Array.zeroCreate (keys + 1)
+    let column_pos = Array.zeroCreate keys //running position calculation of notes for sv
+    let hold_presence = Array.create keys false
+    let hold_pos = Array.create keys 0.0f
+    let hold_colors = Array.create keys 0
 
     do
         //todo: position differently for editor
@@ -30,14 +55,78 @@ type NoteRenderer() as this =
         let playfieldHeight = bottom - top
         let now = Audio.timeWithOffset()
 
+        //seek to appropriate sv and note locations in data.
+        //all of this stuff could be wrapped in an object handling seeking/peeking but would give slower performance because it's based on Seq and not ResizeArray
+        //i therefore sadly had to make a mess here. see comments on the variables for more on whats going on
+        while note_seek < notes.Data.Count && (offsetOf notes.Data.[note_seek]) < now do
+            note_seek <- note_seek + 1
+        note_peek <- note_seek
+        for i in 0 .. keys do
+            while sv_seek.[i] < sv.[i].Count && (offsetOf <| sv.[i].[sv_seek.[i]]) < now do
+                sv_seek.[i] <- sv_seek.[i] + 1
+            sv_peek.[i] <- sv_seek.[i]
+            sv_value.[i] <- if sv_seek.[i] > 0 then snd sv.[i].[sv_seek.[i] - 1] else 1.0f
+
         for k in 0 .. (keys - 1) do
             Draw.rect(Rect.create (left + columnPositions.[k]) top (left + columnPositions.[k] + columnWidths.[k]) bottom) (Color.FromArgb(127, 0, 0, 0)) Sprite.Default
+            sv_time.[k] <- now
+            column_pos.[k] <- hitposition
+            hold_pos.[k] <- hitposition
+            hold_presence.[k] <-
+                if note_seek > 0 then
+                    let (_, struct (nd, c)) = notes.Data.[note_seek - 1] in
+                    hold_colors.[k] <- int c.[k]
+                    (testForNote k NoteType.HOLDHEAD nd || testForNote k NoteType.HOLDBODY nd)
+                else false
 
-        for (time, nd) in notes.EnumerateBetween(now - 200.0) (now + 1500.0) do //ranges
-            let pos = bottom - hitposition - float32 ((time - now) * scale)
+        //main render loop - until the last note rendered in every column appears off screen
+        let mutable min = hitposition
+        while min < playfieldHeight && note_peek < notes.Data.Count do
+            min <- playfieldHeight
+            let (t, struct (nd, color)) = notes.Data.[note_peek]
+            //until no sv adjustments needed...
+            //update main sv
+            while (sv_peek.[0] < sv.[0].Count && offsetOf sv.[0].[sv_peek.[0]] < t) do
+                let (t2, v) = sv.[0].[sv_peek.[0]]
+                for k in 0 .. (keys - 1) do
+                    column_pos.[k] <- column_pos.[k] + scale * sv_value.[0] * (t2 - sv_time.[k])
+                    sv_time.[k] <- t2
+                sv_value.[0] <- v
+                sv_peek.[0] <- sv_peek.[0] + 1
+            //update column sv
+
+            //render notes
             for k in 0 .. (keys - 1) do
+                column_pos.[k] <- column_pos.[k] + scale * sv_value.[0] * (t - sv_time.[k])
+                sv_time.[k] <- t
+                min <- Math.Min(column_pos.[k], min)
                 if testForNote k NoteType.NORMAL nd then
-                    Draw.rect (Rect.create(left + columnPositions.[k]) (pos - noteHeight) (left + columnPositions.[k] + columnWidths.[k]) pos) Color.White <| Themes.getTexture("note")
+                    let pos = bottom - column_pos.[k] //SCROLL
+                    Draw.quad (Quad.ofRect (Rect.create(left + columnPositions.[k]) (pos - noteHeight) (left + columnPositions.[k] + columnWidths.[k]) pos)) (Quad.colorOf Color.White) (Sprite.uv(0, int color.[k])(Themes.getTexture("note"))) //ANIMATION
+                elif testForNote k NoteType.HOLDHEAD nd then
+                    hold_pos.[k] <- column_pos.[k]
+                    hold_colors.[k] <- int color.[k]
+                    hold_presence.[k] <- true
+                elif testForNote k NoteType.HOLDTAIL nd then
+                    let headpos = bottom - hold_pos.[k]
+                    let pos = bottom - column_pos.[k] + holdnoteTrim //SCROLL
+                    if headpos > pos then //SCROLL
+                        Draw.quad (Quad.ofRect (Rect.create(left + columnPositions.[k]) (pos - noteHeight * 0.5f) (left + columnPositions.[k] + columnWidths.[k]) (headpos - noteHeight * 0.5f))) (Quad.colorOf Color.White) (Sprite.uv(0, hold_colors.[k])(Themes.getTexture("holdbody"))) //ANIMATION
+                    if pos - headpos < noteHeight * 0.5f then
+                        Draw.quad
+                            (Quad.ofRect (Rect.create(left + columnPositions.[k]) (pos - noteHeight) (left + columnPositions.[k] + columnWidths.[k]) (Math.Min(pos, headpos - noteHeight * 0.5f))))
+                            (Quad.colorOf Color.White)
+                            (Sprite.uv(0, int color.[k])(Themes.getTexture("holdtail"))) //ANIMATION
+                    Draw.quad (Quad.ofRect (Rect.create(left + columnPositions.[k]) (headpos - noteHeight) (left + columnPositions.[k] + columnWidths.[k]) headpos)) (Quad.colorOf Color.White) (Sprite.uv(0, hold_colors.[k])(Themes.getTexture("holdhead"))) //ANIMATION
+                    hold_presence.[k] <- false
+            note_peek <- note_peek + 1
+        
+        for k in 0 .. (keys - 1) do
+            if hold_presence.[k] then
+                let headpos = bottom - hold_pos.[k]
+                Draw.quad (Quad.ofRect (Rect.create(left + columnPositions.[k]) top (left + columnPositions.[k] + columnWidths.[k]) (headpos - noteHeight * 0.5f))) (Quad.colorOf Color.White) (Sprite.uv(0, hold_colors.[k])(Themes.getTexture("holdbody"))) //ANIMATION
+                Draw.quad (Quad.ofRect (Rect.create(left + columnPositions.[k]) (headpos - noteHeight) (left + columnPositions.[k] + columnWidths.[k]) headpos)) (Quad.colorOf Color.White) (Sprite.uv(0, hold_colors.[k])(Themes.getTexture("holdhead"))) //ANIMATION
+                
             
 
 
