7fccdfa22f7de522acff0fbf35c274fc6e9f22e8
Percyqaz <percyqaz@yahoo.co.uk>
2020-12-06 15:28:29 +0000

    building out options screen starting with window config

--- Interlude/Game.fs
+++ Interlude/Game.fs
@@ -9,22 +9,23 @@ open Interlude.Input
 open Interlude.Options
 open Interlude.UI
 
-type Game(config: GameConfig) =
+type Game(config: GameConfig) as this =
     inherit GameWindow(300, 300, new GraphicsMode(ColorFormat(32), 24, 8, 0))
 
     let screens = new ScreenContainer()
 
     do
+        Options.applyOptions <- fun () -> this.ApplyConfig(config)
         base.Title <- Utils.version
         base.VSync <- VSyncMode.Off
         //base.Cursor <- new MouseCursor.Empty
 
-    member private this.ApplyConfig(config: GameConfig) = 
-        base.TargetRenderFrequency <- config.FrameLimiter
-        match config.WindowMode with
+    member this.ApplyConfig(config: GameConfig) =
+        base.TargetRenderFrequency <- config.FrameLimiter.Get()
+        match config.WindowMode.Get() with
         | WindowType.WINDOWED ->
             base.WindowState <- WindowState.Normal
-            let (resizable, struct (width, height)) = Options.getResolution config.Resolution
+            let (resizable, struct (width, height)) = Options.getResolution <| config.Resolution.Get()
             base.WindowBorder <- if resizable then WindowBorder.Resizable else WindowBorder.Fixed
             base.ClientRectangle <- new Rectangle(0, 0, width, height)
         | WindowType.BORDERLESS ->
