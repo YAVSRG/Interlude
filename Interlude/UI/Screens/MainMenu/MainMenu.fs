76d87515054ca6212633e8f22a78eac241913ab6
Percyqaz <percyqaz@yahoo.co.uk>
2021-10-08 21:37:44 +0100

    split main menu file

--- Interlude/UI/Screens/MainMenu/MainMenu.fs
+++ Interlude/UI/Screens/MainMenu/MainMenu.fs
@@ -1,4 +1,4 @@
-﻿namespace Interlude.UI.Screens
+﻿namespace Interlude.UI.Screens.MainMenu
 
 open System
 open System.Drawing
@@ -12,43 +12,6 @@ open Interlude.UI.Animation
 open Interlude.UI.Components
 open Interlude.UI.OptionsMenu
 open Interlude.Input
-
-// Loading screen
-
-type LoadingScreen() as this =
-    inherit Screen.T()
-
-    let mutable closing = false
-    let fade = new AnimationFade 1.0f
-    do
-        this.Animation.Add fade
-
-    override this.OnEnter (prev: Screen.Type) =
-        fade.Value <- 0.0f
-        Logo.moveCentre ()
-        Screen.toolbar <- true
-        match prev with
-        | Screen.Type.MainMenu ->
-            closing <- true
-            let s = AnimationSequence()
-            s.Add (AnimationTimer 1500.0)
-            s.Add (AnimationAction (fun () -> Screen.back Screen.TransitionFlag.Default))
-            this.Animation.Add s
-        | _ -> 
-            let s = AnimationSequence()
-            s.Add (AnimationTimer 1500.0)
-            s.Add (AnimationAction(fun () -> Screen.change Screen.Type.MainMenu Screen.TransitionFlag.UnderLogo))
-            this.Animation.Add s
-
-    override this.OnExit _ = ()
-
-    override this.Update (elapsedTime, bounds) =
-        base.Update (elapsedTime, bounds)
-        Audio.changeVolume (Options.options.AudioVolume.Value * float (if closing then 1.0f - fade.Value else fade.Value))
-        
-    override this.Draw() =
-        let (x, y) = Rect.center this.Bounds
-        Text.drawJust (Themes.font(), (if closing then "Bye o/" else "Loading :)"), 80.f, x, y - 500.0f, Color.White, 0.5f)
 
 type private MenuButton(onClick, label) as this =
     inherit Widget()
@@ -70,7 +33,7 @@ type private MenuButton(onClick, label) as this =
 
 // Menu screen
 
-type MainMenu() as this =
+type Screen() as this =
     inherit Screen.T()
 
     let playFunc() =
