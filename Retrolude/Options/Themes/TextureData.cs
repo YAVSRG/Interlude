ceeecf151102ef669f1eae2df467cf46950934a1
Percyqaz <>
2021-12-06 12:11:19 +0000

    remove retrolude

--- Retrolude/Options/Themes/TextureData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Interlude.Options.Themes
-{
-    public class TextureData
-    {
-        public int Columns = 1;
-        public int Rows = 1;
-        public bool Tiling = false;
-    }
-}
