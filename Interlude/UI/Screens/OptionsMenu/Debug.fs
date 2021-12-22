ed1f3725e0418dce573d93583305807ad743789e
Percyqaz <>
2021-12-22 11:43:22 +0000

    buttons that start tasks give feedback and can't be clicked again

--- Interlude/UI/Screens/OptionsMenu/Debug.fs
+++ Interlude/UI/Screens/OptionsMenu/Debug.fs
@@ -13,11 +13,19 @@ module Debug =
         {
             Content = fun add ->
                 column [
-                    PrettyButton("RebuildCache", fun () -> BackgroundTask.Create TaskFlags.LONGRUNNING "Rebuilding Cache" Library.rebuildTask |> ignore).Position(200.0f)
-                    PrettyButton("DownloadUpdate",
-                        fun () ->
+                    PrettyButton.Once(
+                        "RebuildCache",
+                        (fun () -> BackgroundTask.Create TaskFlags.LONGRUNNING "Rebuilding Cache" Library.rebuildTask |> ignore),
+                        Localisation.localiseWith ["Rebuilding Cache"] "notification.TaskStarted", Task
+                    ).Position(200.0f)
+                    PrettyButton.Once(
+                        "DownloadUpdate",
+                        ( fun () ->
                             if Interlude.Utils.AutoUpdate.updateAvailable then
                                 Interlude.Utils.AutoUpdate.applyUpdate(fun () -> Notification.add (Localisation.localise "notification.UpdateInstalled", System))
+                        ),
+                        Localisation.localise "notification.UpdateInstalling", System,
+                        Enabled = Interlude.Utils.AutoUpdate.updateAvailable
                     ).Position(300.0f)
                     PrettyButton("PatternTest", fun () -> Prelude.Editor.Patterns.Analysis.test Interlude.Gameplay.currentChart.Value).Position(400.0f)
                 ] :> Selectable
