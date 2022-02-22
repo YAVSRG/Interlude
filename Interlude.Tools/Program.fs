421db1009b0259cdbb68f3dc19cb2cc4ccc67c1f
Percyqaz <>
2022-02-22 19:13:19 +0000

    🕶 Some build tool stuff

--- Interlude.Tools/Program.fs
+++ Interlude.Tools/Program.fs
@@ -1,8 +1,6 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
+﻿open Interlude.Tools
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    printfn "%s" Utils.YAVSRG_PATH
+    0
---
+++ Interlude.Tools/Utils.fs
@@ -0,0 +1,11 @@
+﻿namespace Interlude.Tools
+
+module Utils =
+    open System.IO
+
+    let YAVSRG_PATH = 
+        Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "..")
+        |> Path.GetFullPath
+
+    let ASSETS_PATH = Path.Combine(YAVSRG_PATH, "Interlude.Assets")
+    let BUILD_RESOURCES_PATH = Path.Combine(YAVSRG_PATH, "Interlude", "Resources")
