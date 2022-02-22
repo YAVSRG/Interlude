0487eb4c75ac5f3d82a04f14cb0d259edd075ec9
Percyqaz <>
2022-02-22 20:05:26 +0000

    🤠 Created script to bundle game assets - copies a bunch of prefabricated rulesets into theme too - rulesets are now loaded straight from default theme

--- Interlude.Tools/Program.fs
+++ Interlude.Tools/Program.fs
@@ -1,6 +1,12 @@
-﻿open Interlude.Tools
+﻿open System
+open Interlude.Tools
 
 [<EntryPoint>]
 let main argv =
     printfn "%s" Utils.YAVSRG_PATH
+
+    printfn "Press enter to bundle assets"
+    Console.ReadLine() |> ignore
+    Features.Bundle_Assets.main()
+
     0
