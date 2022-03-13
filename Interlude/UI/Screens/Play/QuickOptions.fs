dbd1f2617ee87d4c5d5c2750d10eb496a999527e
Percyqaz <>
2022-03-13 15:00:36 +0000

    🤝 Add "apply recommended offset" button

--- Interlude/UI/Screens/Play/QuickOptions.fs
+++ Interlude/UI/Screens/Play/QuickOptions.fs
@@ -12,6 +12,7 @@ open Interlude.UI.Components.Selection.Menu
 module QuickOptions =
 
     let page(scoring : Prelude.Scoring.IScoreMetric, callback) : SelectionPage =
+
         let mutable sum = 0.0f<ms>
         let mutable count = 1.0f
         for ev in scoring.HitEvents do
@@ -21,27 +22,30 @@ module QuickOptions =
                 count <- count + 1.0f
             | _ -> ()
         let mean = sum / count * Gameplay.rate.Value
+
+        let firstNote = Gameplay.currentChart.Value.FirstNote
+        let offset = 
+            Setting.make
+                (fun v -> Gameplay.chartSaveData.Value.Offset <- toTime v + firstNote; Audio.changeLocalOffset(toTime v))
+                (fun () -> float (Gameplay.chartSaveData.Value.Offset - firstNote))
+            |> Setting.bound -200.0 200.0
+            |> Setting.round 0
+        let recommendedOffset = float (Gameplay.chartSaveData.Value.Offset - firstNote - mean)
+
         {
             Content = fun add ->
-                let firstNote = Gameplay.currentChart.Value.FirstNote
                 column [
-                    PrettySetting("quick.localoffset",
-                        Slider(
-                            Setting.make
-                                (fun v -> Gameplay.chartSaveData.Value.Offset <- toTime v + firstNote; Audio.changeLocalOffset(toTime v))
-                                (fun () -> float (Gameplay.chartSaveData.Value.Offset - firstNote))
-                            |> Setting.bound -200.0 200.0
-                            |> Setting.round 0, 0.01f)
-                    ).Position(200.0f)
+                    PrettySetting("quick.localoffset", Slider(offset, 0.01f)).Position(200f)
                     TextBox(
-                        (if Time.Abs mean > 1.0f<ms> then K (sprintf "Suggested: %.0f" (Gameplay.chartSaveData.Value.Offset - firstNote - mean))
-                        else K ""),
+                        K (sprintf "Suggested: %.0f" recommendedOffset),
                         K (Color.White, Color.Black),
                         1.0f
                     ) |> positionWidget(-600.0f, 1.0f, 200.0f, 0.0f, -50.0f, 1.0f, 200.0f + PRETTYHEIGHT, 0.0f)
-                    PrettySetting("gameplay.scrollspeed", Slider<_>.Percent(options.ScrollSpeed, 0.0025f)).Position(280.0f)
-                    PrettySetting("gameplay.hitposition", Slider(options.HitPosition, 0.005f)).Position(360.0f)
-                    PrettySetting("gameplay.upscroll", Selector<_>.FromBool options.Upscroll).Position(440.0f)
+                    PrettyButton("quick.applyoffset", fun () -> offset.Value <- recommendedOffset).Position(280f)
+
+                    PrettySetting("gameplay.scrollspeed", Slider<_>.Percent(options.ScrollSpeed, 0.0025f)).Position(380.0f)
+                    PrettySetting("gameplay.hitposition", Slider(options.HitPosition, 0.005f)).Position(460.0f)
+                    PrettySetting("gameplay.upscroll", Selector<_>.FromBool options.Upscroll).Position(540.0f)
                 ] :> Selectable
             Callback = callback
         }
