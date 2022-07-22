45b394573375fbf24366c01f7aa43b9a19928f02
Percyqaz <>
2022-07-22 12:20:44 +0100

    🕶 Update build scripts

--- tools/Features/debug.bat
+++ tools/Features/debug.bat
@@ -1,4 +1,4 @@
 @echo off
-set build="C:\Users\percy\Desktop\Source\YAVSRG\Interlude\Interlude.fsproj"
+set build="C:\Users\percy\Desktop\Source\YAVSRG\Interlude\src\Interlude.fsproj"
 dotnet build -c Debug "%build%"
-"C:\Users\percy\Desktop\Source\YAVSRG\Interlude\bin\Debug\netcoreapp3.1\Interlude.exe"
+"C:\Users\percy\Desktop\Source\YAVSRG\Interlude\src\bin\Debug\netcoreapp3.1\Interlude.exe"
--- tools/Utils.fs
+++ tools/Utils.fs
@@ -4,9 +4,9 @@ module Utils =
     open System.IO
 
     let YAVSRG_PATH = 
-        Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "..")
+        Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "..", "..")
         |> Path.GetFullPath
 
-    let ASSETS_PATH = Path.Combine(YAVSRG_PATH, "Interlude.Assets")
-    let BUILD_RESOURCES_PATH = Path.Combine(YAVSRG_PATH, "Interlude", "Resources")
-    let INTERLUDE = Path.Combine(YAVSRG_PATH, "Interlude")
+    let ASSETS_PATH = Path.Combine(YAVSRG_PATH, "Interlude", "assets")
+    let BUILD_RESOURCES_PATH = Path.Combine(YAVSRG_PATH, "Interlude", "src", "Resources")
+    let INTERLUDE = Path.Combine(YAVSRG_PATH, "Interlude", "src")
