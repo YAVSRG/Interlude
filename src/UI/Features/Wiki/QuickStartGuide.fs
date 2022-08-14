bec0bc4a7e8b6f2c457dc5d9b86ab43febd3fe86
percyqaz <21290233+percyqaz@users.noreply.github.com>
2022-08-14 14:35:30 +0100

    🤠Split quick start guide into feature folder
    + Moved some other features to feature folders

---
+++ src/UI/Features/Wiki/QuickStartGuide.fs
@@ -0,0 +1,34 @@
+﻿namespace Interlude.Features.Wiki
+
+open Percyqaz.Flux.Input
+open Percyqaz.Flux.UI
+open Interlude.Utils
+open FSharp.Formatting.Markdown
+
+// todo: make this read from/integrate with the online wiki
+
+type QuickStartDialog(doc) =
+    inherit Dialog()
+
+    let markdown = MarkdownUI.build doc
+    let flow = ScrollContainer(markdown.Body, K (markdown.Height + markdown.LHeight), Position = Position.Margin(400.0f, 100.0f))
+
+    override this.Init(parent: Widget) =
+        base.Init parent
+        flow.Init this
+
+    override this.Update(elapsedTime, moved) =
+        base.Update(elapsedTime, moved)
+        flow.Update(elapsedTime, moved)
+        if Mouse.leftClick() || (!|"exit").Tapped() then
+            this.Close()
+
+    override this.Draw() = flow.Draw()
+
+module QuickStartGuide =
+
+    let doc = 
+        getResourceText "QuickStart.md"
+        |> Markdown.Parse
+
+    let show_help() = (QuickStartDialog doc).Show()
