e3eb3bbd8b415d7bb82b40cfba585f83e34aaed2
percyqaz <21290233+percyqaz@users.noreply.github.com>
2022-08-20 17:07:55 +0100

    🤠 Restore Group/Sort dropdowns on level select

--- src/UI/Components/Controls.fs
+++ src/UI/Components/Controls.fs
@@ -7,8 +7,7 @@ open Percyqaz.Flux.Graphics
 open Percyqaz.Flux.Input
 open Percyqaz.Flux.UI
 open Prelude.Common
-open Interlude
-open Interlude.Utils
+open Prelude.Data.Charts.Sorting
 open Interlude.UI
 
 type StylishButton(onClick, labelFunc: unit -> string, colorFunc, bind: Hotkey) as this =
@@ -56,3 +55,43 @@ type StylishButton(onClick, labelFunc: unit -> string, colorFunc, bind: Hotkey) 
             (fun () -> sprintf "%s: %s" label names.[i]),
             colorFunc
         )
+
+type TextEntryBox(setting: Setting<string>, bind: Hotkey, prompt: string) as this =
+    inherit Frame(NodeType.Switch(fun _ -> this.TextEntry))
+
+    let textEntry = TextEntry(setting, bind, Position = Position.Margin(10.0f, 0.0f))
+
+    do
+        this
+        |+ textEntry
+        |* Text(
+                fun () ->
+                    match bind with
+                    | "none" -> match setting.Value with "" -> prompt | _ -> ""
+                    | b ->
+                        match setting.Value with
+                        | "" -> Localisation.localiseWith [(!|b).ToString(); prompt] "misc.search"
+                        | _ -> ""
+                ,
+                Color = textEntry.TextColor,
+                Align = Alignment.LEFT,
+                Position = Position.Margin(10.0f, 0.0f))
+
+    member private this.TextEntry = textEntry
+
+    override this.Update(elapsedTime, bounds) =
+        base.Update(elapsedTime, bounds)
+
+type SearchBox(s: Setting<string>, callback: unit -> unit) as this =
+    inherit TextEntryBox(s |> Setting.trigger(fun _ -> this.StartSearch()), "search", "search")
+    let searchTimer = new Diagnostics.Stopwatch()
+
+    member val DebounceTime = 400L with get, set
+
+    new(s: Setting<string>, callback: Filter -> unit) = SearchBox(s, fun () -> callback(Filter.parse s.Value))
+
+    member private this.StartSearch() = searchTimer.Restart()
+
+    override this.Update(elapsedTime, bounds) =
+        base.Update(elapsedTime, bounds)
+        if searchTimer.ElapsedMilliseconds > this.DebounceTime then searchTimer.Reset(); callback()
