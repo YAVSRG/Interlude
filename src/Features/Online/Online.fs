3c283e4e2ef086fa7c38ad3b2aeeec769ed492a3
percyqaz <21290233+percyqaz@users.noreply.github.com>
2023-02-01 16:52:42 +0000

    ⚗️ Upgrade to FSharp.Core 7.0.0 + Experimental web code

---
+++ src/Features/Online/Online.fs
@@ -0,0 +1,16 @@
+﻿namespace Interlude.Features
+
+open Percyqaz.Common
+open Interlude.Web.Shared
+
+module Online =
+
+    let handle_packet(packet: Downstream) =
+        match packet with
+        | Downstream.DISCONNECT reason -> Logging.Info(sprintf "Disconnected from server: %s" reason)
+        | Downstream.CHAT _ -> ()
+        | Downstream.LOGIN_SUCCESS username -> Logging.Info(sprintf "Successfully logged in as %s" username)
+
+    let connect() =
+        Client.init { Address = "127.0.0.1"; Port = 32767; Username = "Percyqaz"; Handle_Packet = handle_packet }
+        Client.connect()
