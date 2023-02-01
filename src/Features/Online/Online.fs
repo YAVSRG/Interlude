fdecab6a13a9e17afcc40486eef3ab5affca2626
percyqaz <21290233+percyqaz@users.noreply.github.com>
2023-02-01 17:51:12 +0000

    💡 Better handshake/user state logic

--- src/Features/Online/Online.fs
+++ src/Features/Online/Online.fs
@@ -5,12 +5,15 @@ open Interlude.Web.Shared
 
 module Online =
 
+    let requested_username = "Percyqaz"
+
     let handle_packet(packet: Downstream) =
         match packet with
         | Downstream.DISCONNECT reason -> Logging.Info(sprintf "Disconnected from server: %s" reason)
-        | Downstream.CHAT _ -> ()
+        | Downstream.HANDSHAKE_SUCCESS -> Client.send(Upstream.LOGIN requested_username)
         | Downstream.LOGIN_SUCCESS username -> Logging.Info(sprintf "Successfully logged in as %s" username)
+        | Downstream.CHAT _ -> ()
 
     let connect() =
-        Client.init { Address = "127.0.0.1"; Port = 32767; Username = "Percyqaz"; Handle_Packet = handle_packet }
+        Client.init { Address = "127.0.0.1"; Port = 32767; Handle_Packet = handle_packet }
         Client.connect()
