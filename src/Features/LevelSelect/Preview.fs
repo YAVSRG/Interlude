3372b38dfca163630045e60a5756f12c133bfbe5
percyqaz <21290233+percyqaz@users.noreply.github.com>
2023-02-07 08:10:51 +0000

    ✨ Improved how preview timeline looks

--- src/Features/LevelSelect/Preview.fs
+++ src/Features/LevelSelect/Preview.fs
@@ -27,18 +27,19 @@ type Preview(chart: Chart) =
     override this.Draw() =
         renderer.Draw()
 
-        let w = this.Bounds.Width / float32 density_graph_1.Length
+        let b = this.Bounds.Shrink(10.0f, 20.0f)
+
+        let w = b.Width / float32 density_graph_1.Length
         for i = 0 to density_graph_1.Length - 1 do
-            let h = 200.0f * density_graph_1.[i] / max_note_density
-            let h2  = 200.0f * density_graph_2.[i] / max_note_density
-            Draw.rect (Rect.Box(this.Bounds.Left + float32 i * w, this.Bounds.Bottom - h, w, h - 10.0f)) (Color.FromArgb(120, Color.White))
-            Draw.rect (Rect.Box(this.Bounds.Left + float32 i * w, this.Bounds.Bottom - h2, w, h2 - 10.0f)) (Style.color(80, 1.0f, 0.0f))
+            let h = 80.0f * density_graph_1.[i] / max_note_density
+            let h2 = 80.0f * density_graph_2.[i] / max_note_density
+            Draw.rect (Rect.Box(b.Left + float32 i * w, b.Bottom - h, w, h - 5.0f)) (Color.FromArgb(120, Color.White))
+            Draw.rect (Rect.Box(b.Left + float32 i * w, b.Bottom - h2, w, h2 - 5.0f)) (Style.color(80, 1.0f, 0.0f))
 
         let percent = (Song.time() - chart.FirstNote) / (chart.LastNote - chart.FirstNote) 
-        Draw.rect (this.Bounds.SliceBottom(10.0f)) (Style.color(255, 0.4f, 0.0f))
-        let x = this.Bounds.Width * percent
-        Draw.rect (this.Bounds.SliceBottom(10.0f).SliceLeft x) (Style.color(255, 1.0f, 0.0f))
-        Draw.rect (Rect.Create(this.Bounds.Left + x - 2.5f, this.Bounds.Bottom - 200.0f, this.Bounds.Left + x + 2.5f, this.Bounds.Bottom)) Color.White
+        Draw.rect (b.SliceBottom(5.0f)) (Color.FromArgb(160, Color.White))
+        let x = b.Width * percent
+        Draw.rect (b.SliceBottom(5.0f).SliceLeft x) (Style.color(255, 1.0f, 0.0f))
 
     override this.Update(elapsedTime, moved) =
         base.Update(elapsedTime, moved)
