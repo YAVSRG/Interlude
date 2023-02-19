3ba566227884a7dc39e376c2b70ac25c8cc3cc22
percyqaz <21290233+percyqaz@users.noreply.github.com>
2023-02-19 01:10:43 +0000

    🚧 Hide WIP online features for release

--- src/Features/Toolbar/Toolbar.fs
+++ src/Features/Toolbar/Toolbar.fs
@@ -52,7 +52,7 @@ type Toolbar() =
                 Hotkey = "wiki",
                 HoverIcon = Icons.wiki2)
             )
-        |+ Multiplayer.Status(Position = Position.SliceTop(HEIGHT).SliceRight(300.0f))
+        //|+ Multiplayer.Status(Position = Position.SliceTop(HEIGHT).SliceRight(300.0f))
         |+ HotkeyAction("screenshot", fun () ->
             let id =  DateTime.Now.ToString("yyyy'-'MM'-'dd'.'HH'_'mm'_'ss.fffffff") + ".png"
             let path = Path.Combine(getDataPath "Screenshots", id)
