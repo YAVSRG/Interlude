858745bb8e1e37e04744f85885f8758acd9f38d2
percyqaz <21290233+percyqaz@users.noreply.github.com>
2023-08-27 19:27:42 +0100

    ✨ Further improvements to pattern search

--- src/Features/LevelSelect/LevelSelect.fs
+++ src/Features/LevelSelect/LevelSelect.fs
@@ -31,7 +31,7 @@ type LevelSelectScreen() =
 
     let random_chart() =
         if options.AdvancedRecommendations.Value then
-            match Suggestion.get_suggestion Chart.current.Value Chart.cacheInfo.Value with
+            match Suggestion.get_suggestion Chart.current.Value Chart.cacheInfo.Value Tree.filter with
             | Some c -> Tree.switchChart(c, LibraryContext.None, ""); refresh()
             | None -> ()
         else
