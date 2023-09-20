7028faf4adddcc392296f65b25c20c6fd0475871
percyqaz <21290233+percyqaz@users.noreply.github.com>
2023-09-20 22:43:13 +0100

    ✨ osu! score importing, including collecting scores on rate charts together

--- src/Features/Import/Mounts.fs
+++ src/Features/Import/Mounts.fs
@@ -240,7 +240,30 @@ module Mounts =
         let mutable chart_count = 0
         let mutable score_count = 0
 
-        for beatmap_data in main_db.Beatmaps |> Seq.filter (fun b -> b.Mode = 3uy) do
+        let find_matching_chart (beatmap_data: OsuDatabase_Beatmap) (chart: Chart) =
+            let chart_hash = Chart.hash chart
+            match Cache.by_hash chart_hash Library.cache with
+            | None -> 
+                match ``osu!``.detect_rate_mod beatmap_data.Difficulty with
+                | Some rate ->
+                    let chart = Chart.scale rate chart
+                    let chart_hash = Chart.hash chart
+                    match Cache.by_hash chart_hash Library.cache with
+                    | None -> 
+                        Logging.Warn(sprintf "%s [%s] is %.2fx, couldn't find a matching 1.0x so skipping" beatmap_data.TitleUnicode beatmap_data.Difficulty rate)
+                        None
+                    | Some _ ->
+                        Some (chart, chart_hash, rate)
+                | None -> 
+                    Logging.Warn(sprintf "%s [%s] skipped" beatmap_data.TitleUnicode beatmap_data.Difficulty)
+                    None
+            | Some _ -> Some (chart, chart_hash, 1.0f)
+
+        for beatmap_score_data in scores.Beatmaps |> Seq.where (fun b -> b.Scores.Length > 0 && b.Scores.[0].Mode = 3uy) do
+            match main_db.Beatmaps |> Seq.tryFind (fun b -> b.Hash = beatmap_score_data.Hash && b.Mode = 3uy) with
+            | None -> ()
+            | Some beatmap_data ->
+
             let osu_file = Path.Combine(m.SourceFolder, beatmap_data.FolderName, beatmap_data.Filename)
             match
                 try
@@ -252,45 +275,47 @@ module Mounts =
             | None -> ()
             | Some chart ->
 
-            let chart_hash = Chart.hash chart
-
-            match Cache.by_hash chart_hash Library.cache with
-            | None -> 
-                let rm = ``osu!``.detect_rate_mod beatmap_data.Difficulty
-                Logging.Warn(sprintf "Scores for %s [%s] not imported - Detected rate: %A" beatmap_data.TitleUnicode beatmap_data.Difficulty rm)
-            | Some _ ->
+            match find_matching_chart beatmap_data chart with
+            | None -> ()
+            | Some (chart, _, rate) ->
 
             chart_count <- chart_count + 1
 
-            match scores.Beatmaps |> Seq.tryFind (fun b -> b.Hash = beatmap_data.Hash) with
-            | Some scores ->
-                for score in scores.Scores do
-                    let replay_file = Path.Combine(m.SourceFolder, "..", "Data", "r", sprintf "%s-%i.osr" score.BeatmapHash score.Timestamp)
-                    use file = File.OpenRead replay_file
-                    use br = new BinaryReader(file)
-                    let replay_info = ScoreDatabase_Score.Read br
+            for score in beatmap_score_data.Scores do
+                let replay_file = Path.Combine(m.SourceFolder, "..", "Data", "r", sprintf "%s-%i.osr" score.BeatmapHash score.Timestamp)
+                match
+                    try
+                        use file = File.OpenRead replay_file
+                        use br = new BinaryReader(file)
+                        Some (ScoreDatabase_Score.Read br)
+                    with err -> Logging.Error(sprintf "Error loading replay file %s" replay_file, err); None
+                with
+                | None -> ()
+                | Some replay_info ->
 
-                    match Mods.to_interlude_rate_and_mods replay_info.ModsUsed with
-                    | None -> () // score is invalid for import in some way, skip
-                    | Some (rate, mods) ->
+                match Mods.to_interlude_rate_and_mods replay_info.ModsUsed with
+                | None -> () // score is invalid for import in some way, skip
+                | Some (rate2, mods) ->
 
-                    let replay_data = decode_replay (replay_info, chart)
+                let combined_rate = rate2 * rate
+                if MathF.Round(combined_rate, 3) <> MathF.Round(combined_rate, 2) || combined_rate > 2.0f || combined_rate < 0.5f then
+                    Logging.Info(sprintf "Skipping score with rate %.3f because this isn't supported in Interlude" combined_rate)
+                else
 
-                    let score : Score = 
-                        { 
-                            time = DateTime.FromFileTimeUtc(replay_info.Timestamp)
-                            replay = Replay.compress replay_data
-                            rate = rate
-                            selectedMods = mods
-                            layout = Layout.Layout.LeftTwo
-                            keycount = chart.Keys
-                        }
+                let replay_data = decode_replay (replay_info, chart, rate)
 
-                    let data = Scores.getOrCreateData chart
-                    if data.Scores |> Seq.exists (fun s -> s.time.Ticks = score.time.Ticks) then () // skip
-                    else
-                        score_count <- score_count + 1
-                        data.Scores.Add(score)
+                let score : Score = 
+                    { 
+                        time = DateTime.FromFileTimeUtc(replay_info.Timestamp).ToLocalTime()
+                        replay = Replay.compress replay_data
+                        rate = MathF.Round(combined_rate, 2)
+                        selectedMods = mods
+                        layout = Layout.Layout.LeftTwo
+                        keycount = chart.Keys
+                    }
 
-            | None -> ()
+                let data = Scores.getOrCreateData chart
+                if data.Scores.RemoveAll(fun s -> s.time.ToUniversalTime().Ticks = score.time.ToUniversalTime().Ticks) = 0 then score_count <- score_count + 1
+                data.Scores.Add(score) 
+
         Logging.Info(sprintf "Finished importing osu! scores (%i scores from %i maps)" score_count chart_count)
