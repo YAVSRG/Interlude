687b5b4d4b655ee3939f680a65705a678c28c160
percyqaz <21290233+percyqaz@users.noreply.github.com>
2023-09-24 14:30:22 +0100

    🧱 Hidden WIP noteskin editing stuff

--- src/Features/Options/Noteskins/Editor.fs
+++ src/Features/Options/Noteskins/Editor.fs
@@ -9,10 +9,50 @@ open Prelude.Common
 open Prelude.Data.Content
 open Interlude.Content
 open Interlude.Options
-open Interlude.Utils
-open Interlude.UI
 open Interlude.UI.Menu
 open Interlude.UI.Components
+
+type NoteTextureEditPage() as this =
+    inherit Page()
+
+    let sprite = getTexture "note"
+    let stitched = Setting.simple false
+
+    do
+        this.Content(
+            column()
+            |+ PageSetting("noteskins.edit.holdnotetrim", Selector<_>.FromBool(stitched))
+                .Pos(170.0f)
+                .Tooltip(Tooltip.Info("noteskins.edit.holdnotetrim"))
+        )
+
+    override this.Draw() =
+        base.Draw()
+        let t = this.Bounds.Top + 50.0f
+        let l = this.Bounds.Right - 50.0f - 64.0f * float32 sprite.Columns
+        for x = 0 to sprite.Columns - 1 do
+            for y = 0 to sprite.Rows - 1 do
+                Draw.quad (Rect.Box(l + 64.0f * float32 x, t + 64.0f * float32 y, 64.0f, 64.0f) |> Quad.ofRect) (Quad.colorOf Color.White) (Sprite.gridUV (x, y) sprite)
+        
+    override this.Title = "WIP"
+    override this.OnClose() = ()
+
+type TextureCard(id: string, on_click: unit -> unit) as this =
+    inherit Frame(NodeType.Button (fun () -> Style.click.Play(); on_click()),
+        Fill = (fun () -> if this.Focused then Colors.pink.O2 else Colors.shadow_2.O2),
+        Border = (fun () -> if this.Focused then Colors.pink_accent else Colors.grey_2.O3))
+
+    let sprite = getTexture id
+
+    do
+        this
+        |+ Text(id,
+            Align = Alignment.CENTER,
+            Position = Position.Margin(Style.PADDING).SliceBottom(50.0f))
+        |+ Image(sprite, Position = Position.Margin(50.0f))
+        |* Clickable.Focus this
+
+    override this.OnFocus() = Style.hover.Play(); base.OnFocus()
 
 type NoteColorPicker(color: Setting<byte>) as this =
     inherit StaticContainer(NodeType.Leaf)
@@ -74,43 +114,57 @@ type EditNoteskinPage() as this =
 
     do
         this.Content(
-            column()
-            |+ PageSetting("noteskins.edit.noteskinname", TextEntry(name, "none"))
-                .Pos(100.0f)
-            |+ PageSetting("noteskins.edit.holdnotetrim", Slider(holdNoteTrim))
-                .Pos(170.0f)
-                .Tooltip(Tooltip.Info("noteskins.edit.holdnotetrim"))
-            |+ PageSetting("noteskins.edit.enablecolumnlight", Selector<_>.FromBool enableColumnLight)
-                .Pos(240.0f)
-                .Tooltip(Tooltip.Info("noteskins.edit.enablecolumnlight"))
-            |+ PageSetting("noteskins.edit.columnwidth", Slider(columnWidth, Step = 1f))
-                .Pos(310.0f)
-                .Tooltip(Tooltip.Info("noteskins.edit.columnwidth"))
-            |+ PageSetting("noteskins.edit.columnspacing",Slider(columnSpacing, Step = 1f))
-                .Pos(390.0f)
-                .Tooltip(Tooltip.Info("noteskins.edit.columnspacing"))
-            |+ PageSetting("generic.keymode",
-                    Selector<Keymode>.FromEnum(keycount |> Setting.trigger (ignore >> refreshColors)) )
-                .Pos(490.0f)
-            |+ PageSetting("noteskins.edit.globalcolors",
-                    Selector<_>.FromBool(
-                        Setting.make
-                            (fun v -> noteColors <- { noteColors with UseGlobalColors = v })
-                            (fun () -> noteColors.UseGlobalColors)
-                        |> Setting.trigger (ignore >> refreshColors)) )
-                .Pos(560.0f)
-                .Tooltip(Tooltip.Info("noteskins.edit.globalcolors"))
-            |+ PageSetting("noteskins.edit.colorstyle",
-                    Selector.FromEnum(
-                        Setting.make
-                            (fun v -> noteColors <- { noteColors with Style = v })
-                            (fun () -> noteColors.Style)
-                        |> Setting.trigger (ignore >> refreshColors)) )
-                .Pos(630.0f)
-                .Tooltip(Tooltip.Info("noteskins.edit.colorstyle"))
-            |+ PageSetting("noteskins.edit.notecolors", colors)
-                .Pos(700.0f, Viewport.vwidth - 200.0f, 120.0f)
-                .Tooltip(Tooltip.Info("noteskins.edit.notecolors"))
+            SwitchContainer.Row<Widget>()
+            |+ (
+                column()
+                |+ PageSetting("noteskins.edit.noteskinname", TextEntry(name, "none"))
+                    .Pos(100.0f)
+                |+ PageSetting("noteskins.edit.holdnotetrim", Slider(holdNoteTrim))
+                    .Pos(170.0f)
+                    .Tooltip(Tooltip.Info("noteskins.edit.holdnotetrim"))
+                |+ PageSetting("noteskins.edit.enablecolumnlight", Selector<_>.FromBool enableColumnLight)
+                    .Pos(240.0f)
+                    .Tooltip(Tooltip.Info("noteskins.edit.enablecolumnlight"))
+                |+ PageSetting("noteskins.edit.columnwidth", Slider(columnWidth, Step = 1f))
+                    .Pos(310.0f)
+                    .Tooltip(Tooltip.Info("noteskins.edit.columnwidth"))
+                |+ PageSetting("noteskins.edit.columnspacing",Slider(columnSpacing, Step = 1f))
+                    .Pos(390.0f)
+                    .Tooltip(Tooltip.Info("noteskins.edit.columnspacing"))
+                |+ PageSetting("generic.keymode",
+                        Selector<Keymode>.FromEnum(keycount |> Setting.trigger (ignore >> refreshColors)) )
+                    .Pos(490.0f)
+                |+ PageSetting("noteskins.edit.globalcolors",
+                        Selector<_>.FromBool(
+                            Setting.make
+                                (fun v -> noteColors <- { noteColors with UseGlobalColors = v })
+                                (fun () -> noteColors.UseGlobalColors)
+                            |> Setting.trigger (ignore >> refreshColors)) )
+                    .Pos(560.0f)
+                    .Tooltip(Tooltip.Info("noteskins.edit.globalcolors"))
+                |+ PageSetting("noteskins.edit.colorstyle",
+                        Selector.FromEnum(
+                            Setting.make
+                                (fun v -> noteColors <- { noteColors with Style = v })
+                                (fun () -> noteColors.Style)
+                            |> Setting.trigger (ignore >> refreshColors)) )
+                    .Pos(630.0f)
+                    .Tooltip(Tooltip.Info("noteskins.edit.colorstyle"))
+                |+ PageSetting("noteskins.edit.notecolors", colors)
+                    .Pos(700.0f, Viewport.vwidth - 200.0f, 120.0f)
+                    .Tooltip(Tooltip.Info("noteskins.edit.notecolors"))
+                )
+            //|+ (
+            //    GridContainer<TextureCard>(200.0f, 2, WrapNavigation = false, Spacing = (20.0f, 20.0f), Position = { Position.Margin(50.0f) with Left = 1.0f %- 470.0f })
+            //    |+ TextureCard("note", (fun () -> NoteTextureEditPage().Show()))
+            //    |+ TextureCard("holdhead", (fun () -> NoteTextureEditPage().Show()))
+            //    |+ TextureCard("holdbody", (fun () -> NoteTextureEditPage().Show()))
+            //    |+ TextureCard("holdtail", (fun () -> NoteTextureEditPage().Show()))
+            //    |+ TextureCard("receptor", (fun () -> NoteTextureEditPage().Show()))
+            //    |+ TextureCard("noteexplosion", (fun () -> NoteTextureEditPage().Show()))
+            //    |+ TextureCard("holdexplosion", (fun () -> NoteTextureEditPage().Show()))
+            //    |+ TextureCard("receptorlighting", (fun () -> NoteTextureEditPage().Show()))
+            //    )
         )
         
     override this.Title = data.Name
