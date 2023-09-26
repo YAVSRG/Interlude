363192fa2ab361926e53601539a77c90675e0927
percyqaz <21290233+percyqaz@users.noreply.github.com>
2023-09-27 00:42:09 +0100

    ⚡️ Possible macOS pipeline build

--- tools/Features/Releases.fs
+++ tools/Features/Releases.fs
@@ -51,17 +51,26 @@ module Releases =
 
         exec "git" (sprintf "commit -a -m \"🏷️ Version %s\"" v)
 
-    let build_win64() =
+    type BuildPlatformInfo =
+        {
+            Name: string
+            RuntimeId: string
+            BassLibraryFile: string
+            GLFWLibraryFile: string
+            ExecutableFile: string
+        }
+
+    let build_platform (info: BuildPlatformInfo) =
         
         Directory.SetCurrentDirectory(INTERLUDE_SOURCE_PATH)
 
-        let build_dir = Path.Combine(INTERLUDE_SOURCE_PATH, "bin", "Release", "net7.0", "win-x64")
-        let clean_dir = Path.Combine(YAVSRG_PATH, "Interlude", "releases", "Interlude-win64")
+        let build_dir = Path.Combine(INTERLUDE_SOURCE_PATH, "bin", "Release", "net7.0", info.RuntimeId)
+        let clean_dir = Path.Combine(YAVSRG_PATH, "Interlude", "releases", $"Interlude-{info.Name}")
 
         try Directory.Delete(build_dir, true) with _ -> ()
         try Directory.Delete(clean_dir, true) with _ -> ()
 
-        exec "dotnet" "publish --configuration Release -r win-x64 -p:PublishSingleFile=True --self-contained true"
+        exec "dotnet" $"publish --configuration Release -r {info.RuntimeId} -p:PublishSingleFile=True --self-contained true"
 
         Directory.CreateDirectory clean_dir |> ignore
 
@@ -75,15 +84,18 @@ module Releases =
                 | ".txt" -> File.Copy(file, Path.Combine(target, Path.GetFileName file))
                 | _ -> ()
 
-        File.Copy(Path.Combine(YAVSRG_PATH, "Percyqaz.Flux", "lib", "win-x64", "bass.dll"), Path.Combine(clean_dir, "bass.dll"))
-        File.Copy(Path.Combine(build_dir, "publish", "Interlude.exe"), Path.Combine(clean_dir, "Interlude.exe"))
-        File.Copy(Path.Combine(build_dir, "publish", "glfw3.dll"), Path.Combine(clean_dir, "glfw3.dll"))
+        File.Copy(Path.Combine(YAVSRG_PATH, "Percyqaz.Flux", "lib", info.RuntimeId, info.BassLibraryFile), Path.Combine(clean_dir, info.BassLibraryFile))
+        File.Copy(Path.Combine(build_dir, "publish", info.ExecutableFile), Path.Combine(clean_dir, info.ExecutableFile))
+        File.Copy(Path.Combine(build_dir, "publish", info.GLFWLibraryFile), Path.Combine(clean_dir, info.GLFWLibraryFile))
         copy (Path.Combine(build_dir, "Locale")) (Path.Combine(clean_dir, "Locale"))
 
         printfn "Outputted to: %s" clean_dir
         if File.Exists(clean_dir + ".zip") then File.Delete(clean_dir + ".zip")
         ZipFile.CreateFromDirectory(clean_dir, clean_dir + ".zip")
         printfn "Zipped to: %s.zip" clean_dir
+    
+    let build_osx_arm64() = build_platform { Name = "osx-arm64"; RuntimeId = "osx-arm64"; BassLibraryFile = "libbass.dylib"; GLFWLibraryFile = "libglfw.3.dylib"; ExecutableFile = "Interlude" }
+    let build_win64() = build_platform { Name = "win64"; RuntimeId = "win-x64"; BassLibraryFile = "bass.dll"; GLFWLibraryFile = "glfw3.dll"; ExecutableFile = "Interlude.exe" }
 
     let register(ctx: ShellContext) : ShellContext =
         ctx.WithCommand(
@@ -98,4 +110,8 @@ module Releases =
             "release_win64",
             "Build an Interlude release and zip it for upload",
             build_win64
+        ).WithCommand(
+            "release_osx_arm64",
+            "Build an Interlude release and zip it for upload",
+            build_osx_arm64
         )
