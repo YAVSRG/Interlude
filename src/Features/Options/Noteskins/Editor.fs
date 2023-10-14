2b7f0faa1a3fa847ab0d22a44d28e8a782d45cc7
percyqaz <21290233+percyqaz@users.noreply.github.com>
2023-10-14 12:32:17 +0100

    ✨ Note rotation editor

--- src/Features/Options/Noteskins/Editor.fs
+++ src/Features/Options/Noteskins/Editor.fs
@@ -1,15 +1,11 @@
 ﻿namespace Interlude.Features.OptionsMenu.Noteskins
 
-open Prelude.Charts.Tools.NoteColors
 open Percyqaz.Common
 open Percyqaz.Flux.Graphics
-open Percyqaz.Flux.Input
 open Percyqaz.Flux.UI
 open Prelude.Common
 open Prelude.Data.Content
 open Interlude.Content
-open Interlude.Options
-open Interlude.Features
 open Interlude.Utils
 open Interlude.UI
 open Interlude.UI.Menu
@@ -57,99 +53,6 @@ type TextureCard(id: string, on_click: unit -> unit) as this =
 
     override this.OnFocus() = Style.hover.Play(); base.OnFocus()
 
-// ----
-
-type NoteColorPicker(color: Setting<byte>, style: ColorScheme, index: int) as this =
-    inherit StaticContainer(NodeType.Leaf)
-    
-    let sprite = getTexture "note"
-    let n = byte sprite.Rows
-    
-    let fd() = Setting.app (fun x -> (x + n - 1uy) % n) color; Style.click.Play()
-    let bk() = Setting.app (fun x -> (x + 1uy) % n) color; Style.click.Play()
-    
-    do 
-        this
-        |+ Tooltip(Callout.Normal
-            .Title(sprintf "%s: %O" (L"noteskins.edit.notecolors.name") style)
-            .Body(L (sprintf "noteskins.edit.notecolors.%s.%i" (style.ToString().ToLower()) index)))
-        |* Clickable((fun () -> (if not this.Selected then this.Select()); fd ()), OnHover = fun b -> if b && not this.Focused then this.Focus())
-
-    override this.OnFocus() = Style.hover.Play(); base.OnFocus()
-    
-    override this.Draw() =
-        base.Draw()
-        if this.Selected then Draw.rect this.Bounds Colors.pink_accent.O2
-        elif this.Focused then Draw.rect this.Bounds Colors.yellow_accent.O2
-        Draw.quad (Quad.ofRect this.Bounds) (Quad.colorOf Color.White) (Sprite.gridUV (3, int color.Value) sprite)
-    
-    override this.Update(elapsedTime, moved) =
-        base.Update(elapsedTime, moved)
-            
-        if this.Selected then
-            if (!|"up").Tapped() then fd()
-            elif (!|"down").Tapped() then bk()
-            elif (!|"left").Tapped() then bk()
-            elif (!|"right").Tapped() then fd()
-
-type ColorSettingsPage() as this =
-    inherit Page()
-    
-    let data = Noteskins.Current.config
-    let keycount = Setting.simple options.KeymodePreference.Value
-    let mutable noteColors = data.NoteColors
-    
-    let g keycount i =
-        let k = if noteColors.UseGlobalColors then 0 else int keycount - 2
-        Setting.make
-            (fun v -> noteColors.Colors.[k].[i] <- v)
-            (fun () -> noteColors.Colors.[k].[i])
-
-    let NOTE_WIDTH = 120.0f
-
-    let colors, refreshColors =
-        refreshRow
-            (fun () -> ColorScheme.count (int keycount.Value) noteColors.Style)
-            (fun i k ->
-                let x = -60.0f * float32 k
-                let n = float32 i
-                NoteColorPicker(g keycount.Value i, noteColors.Style, i, 
-                    Position = { Position.Default with Left = 0.5f %+ (x + NOTE_WIDTH * n); Right = 0.5f %+ (x + NOTE_WIDTH * n + NOTE_WIDTH) })
-            )
-
-    do
-        this.Content(
-            column()
-            |+ PageSetting("noteskins.edit.globalcolors",
-                    Selector<_>.FromBool(
-                        Setting.make
-                            (fun v -> noteColors <- { noteColors with UseGlobalColors = v })
-                            (fun () -> noteColors.UseGlobalColors)
-                        |> Setting.trigger (ignore >> refreshColors)) )
-                .Pos(200.0f)
-                .Tooltip(Tooltip.Info("noteskins.edit.globalcolors"))
-            |+ PageSetting("generic.keymode",
-                    Selector<Keymode>.FromEnum(keycount |> Setting.trigger (ignore >> refreshColors)) )
-                .Pos(270.0f)
-            |+ PageSetting("noteskins.edit.colorstyle",
-                    Selector.FromEnum(
-                        Setting.make
-                            (fun v -> noteColors <- { noteColors with Style = v })
-                            (fun () -> noteColors.Style)
-                        |> Setting.trigger (ignore >> refreshColors)) )
-                .Pos(370.0f)
-                .Tooltip(Tooltip.Info("noteskins.edit.colorstyle"))
-            |+ PageSetting("noteskins.edit.notecolors", colors)
-                .Pos(470.0f, Viewport.vwidth - 200.0f, NOTE_WIDTH)
-            )
-        
-    override this.Title = L"noteskins.edit.colors.name"
-    override this.OnClose() =
-        Noteskins.Current.changeConfig
-            { Noteskins.Current.config with
-                NoteColors = noteColors
-            }
-
 type EditNoteskinPage(from_hotkey: bool) as this =
     inherit Page()
 
@@ -162,7 +65,7 @@ type EditNoteskinPage(from_hotkey: bool) as this =
             SwitchContainer.Row<Widget>()
             |+ (
                 column()
-                |+ PageSetting("noteskins.edit.noteskinname", TextEntry(name, "none"))
+                |+ PageTextEntry("noteskins.edit.noteskinname", name)
                     .Pos(200.0f)
 
                 |+ PageButton("noteskins.edit.playfield", fun () -> PlayfieldSettingsPage().Show())
@@ -174,6 +77,9 @@ type EditNoteskinPage(from_hotkey: bool) as this =
                 |+ PageButton("noteskins.edit.colors", fun () -> ColorSettingsPage().Show())
                     .Pos(440.0f)
                     .Tooltip(Tooltip.Info("noteskins.edit.colors"))
+                |+ PageButton("noteskins.edit.rotations", fun () -> RotationSettingsPage().Show())
+                    .Pos(510.0f)
+                    .Tooltip(Tooltip.Info("noteskins.edit.rotations"))
                 )
             //|+ (
             //    GridContainer<TextureCard>(200.0f, 2, WrapNavigation = false, Spacing = (20.0f, 20.0f), Position = { Position.Margin(50.0f) with Left = 1.0f %- 470.0f })
---
+++ src/Features/Options/Noteskins/Rotations.fs
@@ -0,0 +1,98 @@
+﻿namespace Interlude.Features.OptionsMenu.Noteskins
+
+open Prelude.Charts.Tools.NoteColors
+open Percyqaz.Common
+open Percyqaz.Flux.Graphics
+open Percyqaz.Flux.Input
+open Percyqaz.Flux.UI
+open Prelude.Common
+open Prelude.Data.Content
+open Interlude.Content
+open Interlude.Options
+open Interlude.Utils
+open Interlude.UI
+open Interlude.UI.Menu
+
+type RotationPicker(rotation: Setting<float>) as this =
+    inherit StaticContainer(NodeType.Leaf)
+    
+    let sprite = getTexture "note"
+    
+    let fd() = Setting.app (fun x -> (x + 22.5) % 360.0) rotation; Style.click.Play()
+    let bk() = Setting.app (fun x -> (x - 22.5) %% 360.0) rotation; Style.click.Play()
+    
+    do 
+        this
+        |+ Text((fun () -> sprintf "%.1f" rotation.Value),
+            Position = Position.SliceBottom(30.0f),
+            Align = Alignment.LEFT,
+            Color = K Colors.text_subheading)
+        |* Clickable((fun () -> (if not this.Selected then this.Select()); fd ()),
+            OnRightClick = (fun () -> (if not this.Selected then this.Select()); bk ()),
+            OnHover = fun b -> if b && not this.Focused then this.Focus())
+
+    override this.OnFocus() = Style.hover.Play(); base.OnFocus()
+    
+    override this.Draw() =
+        if this.Selected then Draw.rect this.Bounds Colors.pink_accent.O2
+        elif this.Focused then Draw.rect this.Bounds Colors.yellow_accent.O2
+        Draw.quad (Quad.ofRect this.Bounds |> Quad.rotateDeg rotation.Value) (Quad.colorOf Color.White) (Sprite.gridUV (3, 0) sprite)
+        base.Draw()
+    
+    override this.Update(elapsedTime, moved) =
+        base.Update(elapsedTime, moved)
+            
+        if this.Selected then
+            if (!|"up").Tapped() then fd()
+            elif (!|"down").Tapped() then bk()
+            elif (!|"left").Tapped() then bk()
+            elif (!|"right").Tapped() then fd()
+
+type RotationSettingsPage() as this =
+    inherit Page()
+    
+    let data = Noteskins.Current.config
+    let use_rotation = Setting.simple data.UseRotation
+    let keycount = Setting.simple options.KeymodePreference.Value
+    let mutable rotations = data.Rotations
+    
+    let g keycount i =
+        let k = int keycount - 2
+        Setting.make
+            (fun v -> rotations.[k].[i] <- v)
+            (fun () -> rotations.[k].[i])
+        |> Setting.round 1
+
+    let NOTE_WIDTH = 120.0f
+
+    let _rotations, refreshRotations =
+        refreshRow
+            (fun () -> int keycount.Value)
+            (fun i k ->
+                let x = -60.0f * float32 k
+                let n = float32 i
+                RotationPicker(g keycount.Value i,
+                    Position = { Position.Default with Left = 0.5f %+ (x + NOTE_WIDTH * n); Right = 0.5f %+ (x + NOTE_WIDTH * n + NOTE_WIDTH) })
+            )
+
+    do
+        this.Content(
+            column()
+            |+ PageSetting("noteskins.edit.userotation",
+                    Selector<_>.FromBool(use_rotation) )
+                .Pos(200.0f)
+                .Tooltip(Tooltip.Info("noteskins.edit.userotation"))
+            |+ PageSetting("generic.keymode",
+                    Selector<Keymode>.FromEnum(keycount |> Setting.trigger (ignore >> refreshRotations)) )
+                .Pos(270.0f)
+            |+ PageSetting("noteskins.edit.rotations", _rotations)
+                .Pos(370.0f, Viewport.vwidth - 200.0f, NOTE_WIDTH)
+            )
+        
+    override this.Title = L"noteskins.edit.rotations.name"
+    override this.OnClose() =
+        Noteskins.Current.changeConfig
+            { Noteskins.Current.config with
+                Rotations = rotations
+                UseRotation = use_rotation.Value
+            }
